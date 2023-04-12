178f06852ee9e21d0ef40d11098b697906eacc4a
lvzhengde <luizd@sina.com>
2023-04-12 17:18:54 +0800

    添加SystemC模块initiator_top

---
+++ esl/sc/initiator_top.cpp
@@ -0,0 +1,49 @@
+/*
+ * This module performs:
+ *   1. Instantiation of the controller and the MyInitiator 
+ *      and the interconnecting sc_fifo's
+ *   2. Binding of the Interconnect for the components
+*/
+
+#include "initiator_top.h"                         // this header file
+#include "reporting.h"                             // reporting macro helpers
+
+static const char *filename = "initiator_top.cpp"; ///< filename for reporting
+
+/// Constructor
+
+initiator_top::initiator_top    		       
+( sc_core::sc_module_name name                    
+, const unsigned int    ID                        
+) 
+  :sc_module           (name) 	         // module instance name
+  ,top_initiator_socket                  // Init the socket
+      ("top_initiator_socket")             
+  ,m_ID                (ID)              // initiator ID
+  ,m_initiator                           // Init initiator
+    ("m_initiator"                                            
+    ,ID                                  // ID for reporting                                        
+    )
+  ,m_controller                          // Init controller
+    ("m_traffic_gen"                              
+    ,ID                                  // ID for reporting
+    )
+
+{
+  /// Bind ports to m_request_fifo between m_initiator and m_controller
+  m_controller.request_out_port   (m_request_fifo);
+  m_initiator.request_in_port     (m_request_fifo);
+  
+  /// Bind ports to m_response_fifo between m_initiator and m_controller
+  m_initiator.response_out_port   (m_response_fifo);
+  m_controller.response_in_port   (m_response_fifo);
+
+  /// Bind initiator-socket to initiator-socket hierarchical connection 
+  m_initiator.initiator_socket(top_initiator_socket);
+
+  /// Bind int_ptp_i to int_ptp_i hierarchical connection
+  m_controller.int_ptp_i(int_ptp_i);
+}
+
+
+
---
+++ esl/sc/initiator_top.h
@@ -0,0 +1,48 @@
+/*
+ * top for MyInitiator and controller
+ */
+
+#ifndef __INITIATOR_TOP_H__
+#define __INITIATOR_TOP_H__
+
+#include "tlm.h"                                // TLM headers
+#include "MyInitiator.h"               
+#include "controller.h"                  
+
+class initiator_top                          		       
+  : public sc_core::sc_module    
+{
+
+public:
+//Member Methods  
+  initiator_top 	                                   
+  ( sc_core::sc_module_name name                 ///< module name
+  , const unsigned int  ID                       ///< initiator ID
+  );
+
+  
+public:
+  
+//Member Variables/Objects  ====================================================
+  
+  tlm::tlm_initiator_socket< > top_initiator_socket;
+
+  // Port for interrupt request input
+  sc_in<bool> int_ptp_i;
+
+private:
+  
+  typedef tlm::tlm_generic_payload  *gp_ptr;   ///< Generic Payload pointer
+  
+  sc_core::sc_fifo <gp_ptr>  m_request_fifo;   ///< request SC FIFO
+  sc_core::sc_fifo <gp_ptr>  m_response_fifo;  ///< response SC FIFO
+  
+  const unsigned int         m_ID;             ///< initiator ID
+    
+  MyInitiator                m_initiator;      ///< TLM initiator instance
+  controller                 m_controller;     ///< controller instance
+
+};
+
+#endif /* __INITIATOR_TOP_H__ */
+
